b5db87c6eaac64c2f177471928e4d86cea89fd35
Emma Kawczynski <49253947+emmakawczynski@users.noreply.github.com>
2020-07-29 17:47:25 -0400

    Add files via upload

---
+++ BookedPageFinal/BookedPageFinal/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BookedPageFinal
+//
+//  Created by Emma Kawczynski on 2020-07-29.
+//  Copyright © 2020 Emma Kawczynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var OrderNumber: UILabel!
+    
+    @IBOutlet weak var RestuarantName: UILabel!
+    
+    @IBOutlet weak var TableNumber: UILabel!
+    
+    @IBOutlet weak var BookedName: UILabel!
+    
+    @IBOutlet weak var WaitTIme: UILabel!
+    
+    @IBOutlet weak var HomeButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
